53f6a3a8079b72d93690c0c27ddb9ec62f58f1da
bradygaster <bradyg@microsoft.com>
2024-09-11 21:52:42 -0700

    entity framework needs a little help with the decimal data.

--- ContosoOnline.CatalogApi/DataModels/Product.cs
+++ ContosoOnline.CatalogApi/DataModels/Product.cs
@@ -1,9 +1,13 @@
-﻿namespace ContosoOnline.CatalogApi.DataModels;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoOnline.CatalogApi.DataModels;
 
 public class Product
 {
     public int Id { get; set; }
     public required string Name { get; set; }
     public required string Description { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
 }
