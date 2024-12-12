374cbc010efb427154ef03f95f02d15147d07ca1
bradygaster <bradyg@microsoft.com>
2024-12-12 15:32:09 -0800

    removed validator

--- ContosoOnline.Workers.OrderValidator/Program.cs
+++
@@ -1,7 +0,0 @@
-using ContosoOnline.Workers.OrderValidator;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
